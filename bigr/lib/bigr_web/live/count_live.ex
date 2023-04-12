2ad7509f969591ebe358a75802bdd3ca030a377f
Raul Pereira <raul.pereira@gmail.com>
2023-04-11 21:33:40 -0300

    v3: 5.components and slots

--- bigr/lib/bigr_web/live/count_live.ex
+++ bigr/lib/bigr_web/live/count_live.ex
@@ -29,11 +29,11 @@ defmodule BigrWeb.CountLive do
 
     <hr />
 
-    <div class="grid grid-cols-3 gap-4">
+    <.game_grid>
       <%= for t <- 1..12 do %>
         <.game table_number={t} />
       <% end %>
-    </div>
+    </.game_grid>
     """
   end
 
@@ -50,6 +50,16 @@ defmodule BigrWeb.CountLive do
   defp game(assigns) do
     ~H"""
     <div><%= @table_number %></div>
+    """
+  end
+
+  slot :inner_block
+
+  defp game_grid(assigns) do
+    ~H"""
+    <div class="grid grid-cols-3 gap-4">
+      <%= render_slot(@inner_block) %>
+    </div>
     """
   end
 end
